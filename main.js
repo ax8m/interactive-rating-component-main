f8c9688c425ff664a5bdab7e2fb9cc67cdfd43fc
Aiman268 <ayadel268@gmail.com>
2023-02-06 09:08:55 +0300

    finish challenge

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 console.log("Hello world")
 
-// let getRate = document.getElementById("rate")
-// let getNum = getRate.getElementsByTagName("h3").value
-// console.log(getNum)
+function rating(num) {
+    console.log(num)
+    let x = document.getElementById("btn");
+    x.className = "after_choice";
+
+    // instead propraty (disbled) of button
+    let insteadToAbled = document.querySelector("#btn")
+    insteadToAbled.disabled = false
+
+    document.getElementById("btn").addEventListener("click", () => {
+        console.log("hi")
+
+        let fade = document.getElementById("card_1");
+        fade.className = "fade";
+
+        let result = `You selected ${num} out of 5`
+        document.getElementById("result").innerHTML = `<span>${result}</span>`
+        console.log(result)
+
+
+        let appear = document.getElementById("card_2");
+        appear.className = "appear";
+    })
+
+
+
+}
+
+
